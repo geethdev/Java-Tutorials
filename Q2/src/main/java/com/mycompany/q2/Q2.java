24e4a73a806236ff4003d3acfd1daea5820a2cb3
Geeth Induwara <induwarakwg3573@gmail.com>
2023-09-18 07:04:44 +0530

    Add files via upload

---
+++ Q2/src/main/java/com/mycompany/q2/Q2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Q2 {
+
+    public static void main(String[] args) {
+        int num1 = 20;
+        double num2 = 30.5;
+
+        
+        System.out.println("Answer is :" + (num1 + num2));
+    }
+}
