a439ddbd0fcc00fbbf63ac145f85a7cac6f805c3
Geeth Induwara <induwarakwg3573@gmail.com>
2023-09-19 11:11:56 +0530

    Add files via upload

---
+++ Q6/src/main/java/com/mycompany/q6/Q6.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q6;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Q6 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter first name : ");
+        String fname = input.next();
+        
+        System.out.print("Enter first name : ");
+        String lname = input.next();
+ 
+        System.out.println("Your name is : " + fname + " " + lname);
+    }
+}
