e1f53216eb512a13990e27165072418b59a32df3
Geeth Induwara <induwarakwg3573@gmail.com>
2023-09-21 20:28:41 +0530

    Add files via upload

---
+++ Q9/src/main/java/com/mycompany/q9/Q9.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q9;
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class Q9 {
+
+    public static void main(String[] args) {
+        
+        class Bicycle {
+            private int gear = 5;
+            
+            public void breaking() {
+                System.out.println("Working of breaking");
+            }
+        }
+        
+        Bicycle objBicycle = new Bicycle();
+        objBicycle.breaking();
+        
+    }
+    
+    
+    
+    
+    
+}
