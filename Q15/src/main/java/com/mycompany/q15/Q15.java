88130aa5761b1af118463af6ff8dc4ec87ffa945
Geeth Induwara <induwarakwg3573@gmail.com>
2024-01-02 23:19:24 +0530

    Add files via upload

---
+++ Q15/src/main/java/com/mycompany/q15/Q15.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q15;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Q15 {
+
+    public static void main(String[] args) {
+        
+    }
+}
